80d3e031d9742b86f190958c0d2fc450d549db48
HugoNC <hugonaaman@gmail.com>
2023-08-13 17:44:14 -0300

    Inicia a tratativa de inversão de dependências

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,8 +1,7 @@
-import { prisma } from '@/lib/prisma'
 import { z } from 'zod'
 import { FastifyRequest, FastifyReply } from 'fastify'
-import { hash } from 'bcryptjs'
-import { registerUseCase } from '@/use-cases/register'
+import { RegisterUseCase } from '@/use-cases/register'
+import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
 
 
 export async function register(request: FastifyRequest, reply: FastifyReply) {
@@ -15,7 +14,10 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
   const { email, name, password } = registerBodySchema.parse(request.body)
 
   try {
-    await registerUseCase({
+    const prismaUsersRepository = new PrismaUsersRepository()
+    const registerUseCase = new RegisterUseCase(prismaUsersRepository)
+
+    await registerUseCase.execute({
       name, email, password
     })
   } catch (err) {
