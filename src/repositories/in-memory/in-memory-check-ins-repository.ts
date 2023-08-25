01e1c7a1027d80d5f2f2dc83f33c55c92a3a7cc9
HugoNCastro <hugonaaman@gmail.com>
2023-08-25 15:12:39 -0300

    Cria caso de uso para validação de check-in

--- src/repositories/in-memory/in-memory-check-ins-repository.ts
+++ src/repositories/in-memory/in-memory-check-ins-repository.ts
@@ -35,6 +35,16 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
       .slice((page - 1) * 20, page * 20)
   }
 
+  async findById(id: string){
+    const checkIn = this.items.find(item => item.id === id)
+    
+    if(!checkIn){
+      return null
+    }
+    
+    return checkIn
+  }
+
   async create(data: Prisma.CheckInUncheckedCreateInput): Promise<CheckIn> {
     const checkin = {
       id: randomUUID(),
@@ -47,5 +57,15 @@ export class InMemoryCheckInsRepository implements CheckInsRepository {
     this.items.push(checkin);
 
     return checkin
+  }
+
+  async save(checkIn: CheckIn)  {
+    const checkInIndex = this.items.findIndex(item => item.id === checkIn.id)
+
+    if(checkInIndex >= 0){
+      this.items[checkInIndex] = checkIn
+    }
+
+    return checkIn
   }
 }
