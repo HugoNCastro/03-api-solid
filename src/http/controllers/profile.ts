e9277f8cd77c5efd1b14375f1be4fea3a66802be
HugoNCastro <hugonaaman@gmail.com>
2023-09-05 13:46:09 -0300

    Cria teste para autenticação e para busca de perfil

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,9 +1,17 @@
-import { FastifyRequest, FastifyReply } from 'fastify'
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use-case'
+import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  await request.jwtVerify()
-  // 1 - buscar token dentro dos headers, caso não existe já é retornado erro 500
-  // 2 - se esse token existir nos headers, ela verifica se foi gerado pela nossa aplicação | request.user os dados contidos no token ficam aqui dentro
+  const getUserProfile = makeGetUserProfileUseCase()
 
-  return reply.status(200).send()
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub,
+  })
+
+  return reply.status(200).send({
+    user: {
+      ...user,
+      password_hash: undefined,
+    },
+  })
 }
