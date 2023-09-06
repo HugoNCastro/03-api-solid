450e216eaa427c0ab38745b06f68e38c0b611c72
HugoNCastro <hugonaaman@gmail.com>
2023-09-06 11:15:12 -0300

    Cria controllers para entidade check-ins e cria rotas respectivas

---
+++ src/http/controllers/check-ins/history.ts
@@ -0,0 +1,23 @@
+import { z } from 'zod'
+import { FastifyRequest, FastifyReply } from 'fastify'
+import { makeFecthUserCheckInsHistory } from '@/use-cases/factories/make-fetch-user-check-ins-history-use-case'
+
+
+export async function history(request: FastifyRequest, reply: FastifyReply) {
+  const historyCheckInQuerySchema = z.object({
+    page: z.coerce.number().min(1).default(1)
+  })
+
+  const { page } = historyCheckInQuerySchema.parse(request.query)
+
+  const fecthUserCheckInsHistory = makeFecthUserCheckInsHistory()
+
+  const { checkIns } = await fecthUserCheckInsHistory.execute({
+    userId: request.user.sub,
+    page
+  })
+
+  return reply.status(201).send({
+    checkIns
+  })
+}
